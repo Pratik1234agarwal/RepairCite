3a0bf0b8e01ec0a6f32568f09ad16aac8eb00099
Pratik Agarwal <pratik1234agarwal@gamil.com>
2020-12-17 21:40:37 +0530

    Added build files

---
+++ client/build/precache-manifest.dc315a5ee27aab1aca15c46df922e2db.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9babff6e2ab8a244bdd76dd223d09a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5832f93ee7d964e6b5e1",
+    "url": "/static/css/main.8f01d564.chunk.css"
+  },
+  {
+    "revision": "a42600d3885e40375897",
+    "url": "/static/js/2.f09f73c8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.f09f73c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5832f93ee7d964e6b5e1",
+    "url": "/static/js/main.1e02513f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "2fdabc2cd9e16d2309c7c28c495fb230",
+    "url": "/static/media/Tick.2fdabc2c.png"
+  },
+  {
+    "revision": "4d2c1b88cb6814114cbb5afda942b96e",
+    "url": "/static/media/charging.4d2c1b88.png"
+  },
+  {
+    "revision": "410d3b2cdd3fdfc9709683ff66a46352",
+    "url": "/static/media/landing-img.410d3b2c.jpg"
+  },
+  {
+    "revision": "1a8f6cdf7ce34b9d2663a17deec2b1aa",
+    "url": "/static/media/repair.1a8f6cdf.png"
+  },
+  {
+    "revision": "8c4f4d4bfaa956e1487a380e3745c107",
+    "url": "/static/media/screen.8c4f4d4b.jpg"
+  }
+]);
